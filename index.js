a50cbf1018e0ed3acae0f1cef21cc5da3bdf8022
Ayush Goyal <1ayushgoyal007@gmail.com>
2020-08-06 10:30:22 +0530

    testing api

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ var app = express();
 app.set("view engine","ejs");
 app.use(express.static(__dirname+"/public"));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function(req,res){
     res.render('index');
 })
